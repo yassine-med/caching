f3c68b217dfeb8ea4b76c863a5d07d4535896d26
Yassine <yassine.med.ajil@gmail.com>
2025-07-20 12:56:30 +0100

    Redis conf

---
+++ src/main/java/com/mc/caching/config/CacheConfig.java
@@ -0,0 +1,31 @@
+package com.mc.caching.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(1L))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext
+                        .SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer())
+                );
+
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
--- src/main/java/com/mc/caching/controller/ProductController.java
+++ src/main/java/com/mc/caching/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.mc.caching.controller;
 import com.mc.caching.dto.ProductDTO;
 import com.mc.caching.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
+    private final CacheManager cacheManager;
 
     @PostMapping
     public ResponseEntity<ProductDTO> addProduct(ProductDTO productDTO) {
@@ -31,6 +33,11 @@ public class ProductController {
     public ResponseEntity<Void> deleteProduct(@PathVariable("productId") Long productId) {
         productService.deleteProduct(productId);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/cacheManager")
+    public void cacheManager(){
+        System.out.println(cacheManager);
     }
 
 }
